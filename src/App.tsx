6c4b8cc3650548004095a2b7eea5b29c83888f9a
ackmanx <ackmanx@users.noreply.github.com>
2023-03-19 12:52:44 -0500

    Better app icon. Using JSON5 for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { useEffect } from "react";
 import AppIcon from './app-icon.png'
+import JSON5 from 'json5'
 
 function App() {
   useEffect(() => {
     async function getJson() {
-      const response = await fetch('https://ackmanx.github.io/json/money.json')
-      console.log(777, await response.json())
+      const response = await fetch('https://ackmanx.github.io/json/money.json5')
+      console.log(777, JSON5.parse(await response.text()))
     }
 
     getJson()
