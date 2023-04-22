81e8d7ef25370c3b4852b5ce1298498bbc0ca443
ackmanx <ackmanx@users.noreply.github.com>
2023-04-22 09:57:57 -0500

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,7 @@ import { MoneyResponse } from './money-api-types'
 import { Overview } from './pages/Overview'
 
 function App() {
-  // const [currentTabIndex, setCurrentTabIndex] = useState(getQueryParam('tabIndex') ?? 0)
   const [moneyData, setMoneyData] = useState<MoneyResponse>()
-
-  // const pages = [
-  //   { name: 'Overview', component: <Overview incomes={} loans={} subscriptions={} utilities={} misc={} />},
-  //   { name: 'Debt', component: <Loans loans={moneyData?.loans} /> },
-  //   // { name: 'Loans', component: <Loans loans={moneyData?.loans} /> },
-  //   // { name: 'Credit Cards', component: <CreditCards cards={moneyData?.creditCards} /> },
-  //   // { name: 'Healthcare', component: null },
-  //   // { name: 'People', component: null },
-  //   { name: 'Ugh', component: null },
-  //   // {
-  //   //   name: 'Subscriptions',
-  //   //   component: <Subscriptions subscriptions={moneyData?.subscriptions} />,
-  //   // },
-  //   // { name: 'Utilities', component: null },
-  //   // { name: 'Investment', component: null },
-  //   // { name: 'Misc', component: null },
-  // ]
 
   useEffect(() => {
     async function getJson() {
@@ -35,20 +17,13 @@ function App() {
     getJson()
   }, [])
 
-  // const handleChange = (event: SyntheticEvent, newTabIndex: number) => {
-  //   setQueryParam('tabIndex', newTabIndex)
-  //   setCurrentTabIndex(newTabIndex)
-  // }
-
   return (
     <>
       <Header />
 
       {moneyData && (
-        <main>
-          <div style={{ marginTop: '16px' }}>
-            <Overview moneyData={moneyData} />
-          </div>
+        <main style={{ margin: '16px' }}>
+          <Overview moneyData={moneyData} />
         </main>
       )}
     </>
